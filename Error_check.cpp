9c29e0c89ab50586ed45876d505759b958d71898
ajit_d_darade <74593429+ajitdarade@users.noreply.github.com>
2023-02-07 15:20:56 +0530

    c++

---
+++ Error_check.cpp
@@ -0,0 +1,15 @@
+//This Files is created for Vunerability check
+
+class ErrorCheck
+{
+    public:
+    ErrorCheck() {}
+    ~ErrorCheck() {}
+    string pwd = "password";
+    void checkErr()
+    {checkerror = 1;}
+    static int checkerror = 0;
+    uint8_t testVariable
+    static testVar;
+    
+};
